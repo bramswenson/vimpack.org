4e22e4d30d87d090b9d34fc9fe61d9b737add669
Bram Swenson <bram@craniumisajar.com>
2011-02-13 16:59:11 +0000

    [ROUTES] set json as default format

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ require 'jsonables'
 VimpackOrg::Application.routes.draw do
   scope '/api' do
     scope '/v1' do
-      get '/scripts/search/:q' => 'scripts#search', :q => /.*/
-      get '/scripts/:name' => 'scripts#show', :name => /[a-zA-Z\.]+/
+      get '/scripts/search/:q' => 'scripts#search', :q => /.*/, :defaults => { :format => :json }
+      get '/scripts/:name' => 'scripts#show', :name => /[a-zA-Z\.]+/, :defaults => { :format => 'json' }
       get '/scripts/:name/:jsonable' => 'scripts#show', :name => /[a-zA-Z\.]+/,
-        :defaults => { :jsonable => :simple }, :constraints => Jsonables::RoutingConstraint.new(Script)
+        :defaults => { :jsonable => :simple, :format => :json }, 
+	:constraints => Jsonables::RoutingConstraint.new(Script)
     end
   end
 end
