8e4b1e146d4bad9c6afadce09c0c5c0c95e7a3b0
Bram Swenson <bram@craniumisajar.com>
2011-02-14 15:12:01 +0000

    [ROUTES] um....need numbers in routes please!

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ VimpackOrg::Application.routes.draw do
   scope '/api' do
     scope '/v1' do
       get '/scripts/search/:q' => 'scripts#search', :q => /.*/, :defaults => { :format => :json }
-      get '/scripts/:name' => 'scripts#show', :name => /[a-zA-Z\.]+/, :defaults => { :format => 'json' }
-      get '/scripts/:name/:jsonable' => 'scripts#show', :name => /[a-zA-Z\.]+/,
+      get '/scripts/:name' => 'scripts#show', :name => /[a-zA-Z0-9\.]+/, :defaults => { :format => 'json' }
+      get '/scripts/:name/:jsonable' => 'scripts#show', :name => /[a-zA-Z0-9\.]+/,
         :defaults => { :jsonable => :simple, :format => :json }, 
 	:constraints => Jsonables::RoutingConstraint.new(Script)
     end
