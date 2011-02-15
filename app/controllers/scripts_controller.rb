691926bd41ab832ee3518fdc5d1ce318468c89f6
Bram Swenson <bram@craniumisajar.com>
2011-02-15 00:31:24 -0600

    [SEARCH] added pagination to search results and added search with only script_types

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -7,11 +7,15 @@ class ScriptsController < ApplicationController
     Script.find_by_name(params[:name]).send(jsonable)
   end
   expose(:searched_scripts) do
+    params[:limit] ||= 100
+    params[:page]  ||= 1
     script_types = params[:script_type].blank? ? Array.new : params[:script_type].split(',')
     script_types = script_types.map { |st| st.gsub('_', ' ') }
     Script.search do
-      keywords(params[:q])
+      keywords(params[:q]) unless params[:q].blank?
       with(:script_type).any_of script_types unless script_types.empty?
+      order_by(:name, :asc)
+      paginate(:page => params[:page], :per_page => params[:limit])
     end.results.map(&:simple_attributes)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ require 'jsonables'
 VimpackOrg::Application.routes.draw do
   scope '/api' do
     scope '/v1' do
+      get '/scripts/search' => 'scripts#search', :defaults => { :format => :json }
       get '/scripts/search/:q' => 'scripts#search', :q => /.*/, :defaults => { :format => :json }
       get '/scripts/:name' => 'scripts#show', :name => /[a-zA-Z0-9\.\-_]+/, :defaults => { :format => 'json' }
       get '/scripts/:name/:jsonable' => 'scripts#show', :name => /[a-zA-Z0-9\.\-_]+/,
         :defaults => { :jsonable => :simple, :format => :json }, 
-	:constraints => Jsonables::RoutingConstraint.new(Script)
+	      :constraints => Jsonables::RoutingConstraint.new(Script)
     end
   end
 end
